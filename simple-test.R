0409229cc5d3c994516fa512a3f3159d6c27e4df
DOSull <david.osullivan@vuw.ac.nz>
2021-12-01 20:16:05 +1300

    added to simple-test.R

--- simple-test.R
+++ simple-test.R
@@ -7,15 +7,32 @@ source("weave-map.R")
 ak <- st_read("data/vax-auckland-20211006.gpkg")
 
 bi_weave <- get_biaxial_weave_unit(spacing = 250, aspect = 0.8, crs = 2193)
-tri_weave <- get_triaxial_weave_unit(spacing = 250, type = "cube", strands = "a|b|c",
-                                     aspect = 0.5, crs = 2193)
-
 w1 <- weave_layer(bi_weave, ak, angle = 30)
-w2 <- weave_layer(tri_weave, ak)
 
 tmap_options(check.and.fix = TRUE)
 tm_shape(w1) + 
   tm_fill(col = "strand", palette = "Set1")
 
-tm_shape(w2) +
-  tm_fill(col = "strand", palette = "Set1")
+
+tri_weave <- get_triaxial_weave_unit(spacing = 100, type = "cube", strands = "ab-|cd-|ef-",
+                                     aspect = 1, crs = 2193)
+w2 <- weave_layer(tri_weave, ak, angle = 15)
+tm_shape(w2) + 
+  tm_fill(col = "strand", style = "cat")
+
+layers <- w2 %>% split(as.factor(w2$strand))
+tm_shape(layers$a, name = "Pākehā") +
+  tm_fill(col = "pEuropean", palette = "Greys", title = "%Pākehā") +
+  tm_shape(layers$b, name = "Māori") + 
+  tm_fill(col = "pMaori", palette = "Reds", title = "%Māori") +
+  tm_shape(layers$c, name = "Pasifika") +
+  tm_fill(col = "pPacific", palette = "Purples", title = "%Pasifika") +
+  tm_shape(layers$d, name = "Asian") +
+  tm_fill(col = "pAsian", palette = "Greens", title = "%Asian") +
+  tm_shape(layers$e, name = "Dose 1") +
+  tm_fill(col = "dose1_uptake", palette = "magma", title = "Dose 1") +
+  tm_shape(layers$f, name = "Dose 2") + 
+  tm_fill(col = "dose2_uptake", palette = "magma", title = "Dose 2")
+
+write_weave_layers(w2, ak, "other-data/w2.gpkg")
+
