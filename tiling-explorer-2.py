0d7a6c40d5f965b4ab82cf0be82e8a49f41b50b2
DOSull <david@southosullivan.com>
2025-02-26 18:53:09 +1300

    marimo app built in the weavingdev environment this time

--- tiling-explorer-2.py
+++ tiling-explorer-2.py
@@ -77,7 +77,7 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo, pals, tile, tile_map_button, tiled_map, vars):
-    mo.stop(not tile_map_button.value, mo.md("### Click the **Tile map!** button below to continue"))
+    mo.stop(not tile_map_button.value, mo.md("### Click the **Tile map!** button to continue and to refresh the map after any design changes"))
     tiled_map.variables = {k: v for k, v in zip(tile.tiles.tile_id, vars.value)}
     tiled_map.colourmaps = {k: v for k, v in zip(vars.value, pals.value)}
     tiled_map.render(legend=False)
